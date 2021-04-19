0c099a80f9aba852f9cbb28d6b2d24bd2cb5dbbc
Alejandro Hernández Cordero <ahcorde@gmail.com>
2021-04-19 20:26:06 +0200

    Changed the build type to ament_python and fixed package to run with ros2 run (#18)
    Signed-off-by: ahcorde <ahcorde@gmail.com>

--- setup.py
+++ setup.py
@@ -1,11 +1,39 @@
-#!/usr/bin/env python
+from setuptools import setup
 
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+package_name = 'rqt_publisher'
 
-d = generate_distutils_setup(
-    packages=['rqt_publisher'],
-    package_dir={'': 'src'}
+setup(
+    name=package_name,
+    version='1.0.5',
+    packages=[package_name],
+    package_dir={'': 'src'},
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name + '/resource',
+            ['resource/Publisher.ui']),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, ['plugin.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    author='Dorian Scholz',
+    maintainer='Dirk Thomas, Dorian Scholz',
+    maintainer_email='dthomas@osrfoundation.org',
+    keywords=['ROS'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description=(
+        'A Python GUI plugin publishing ROS messages.'
+    ),
+    license='BSD',
+    entry_points={
+        'console_scripts': [
+            'rqt_publisher = ' + package_name + '.main:main',
+        ],
+    },
 )
-
-setup(**d)
