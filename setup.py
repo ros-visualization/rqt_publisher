fdca864283f1354c862b5b5a2f3f9db3e3615a89
Chris Lalancette <clalancette@gmail.com>
2024-02-06 20:25:07 -0500

    Add in a test dependency on pytest. (#41)
    While there are no tests currently, this sets it up to
    use pytest for tests in the future.
    
    Signed-off-by: Chris Lalancette <clalancette@gmail.com>

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
         'A Python GUI plugin publishing ROS messages.'
     ),
     license='BSD',
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'rqt_publisher = ' + package_name + '.main:main',
