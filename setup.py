451f933019f45e243768fa61a9a53ac149613975
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2026-05-06 21:53:18 +0200

    2.0.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ package_name = 'rqt_publisher'
 
 setup(
     name=package_name,
-    version='1.10.2',
+    version='2.0.0',
     packages=[package_name],
     package_dir={'': 'src'},
     data_files=[
